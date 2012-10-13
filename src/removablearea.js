931721534440cf8c67c264ea8e6f16c7369498e1
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2012-10-13 16:29:34 +0200

    Create base plugin

---
+++ src/removablearea.js
@@ -0,0 +1,28 @@
+/*
+ * removablearea
+ * https://github.com/krampstudio/jQueryRemovableArea
+ *
+ * Copyright (c) 2012 Bertrand Chevrier
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
