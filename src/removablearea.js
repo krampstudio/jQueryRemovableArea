78dbe256ed122738fdd69fd8c351a36deea461f8
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2012-11-24 21:59:32 +0100

    update indentation and fix removed event triggering

--- src/removablearea.js
+++ src/removablearea.js
@@ -2,47 +2,47 @@
 	"use strict";
 
 	var RemovableArea = {
-        _opts : {
-            label       : 'Supprimer',
-            img         : 'imgs/delete.png',
-            warning     : 'Voulez-vous supprimer cet élément?',
-            hoverClass  : 'half-opac',
+		_opts : {
+			label       : 'Supprimer',
+			img         : 'imgs/delete.png',
+			warning     : 'Voulez-vous supprimer cet élément?',
+			hoverClass  : 'half-opac',
 			ctrlClass	: 'removable-ctrl'
-        },
-        _init: function(options){
+		},
+		_init: function(options){
 			var opts = $.extend(true, {}, RemovableArea._opts, options);
-            return this.each(function() {
+			return this.each(function() {
 				var $elt = $(this);
 				if($elt.css('position') != 'relative'){
-                    $elt.css('position', 'relative');
-                }
-                var $ctrl = $("<img class='"+opts.ctrlClass+"' src='"+opts.img+"' alt='"+opts.label+"' title='"+opts.label+"'>");
-                $ctrl.css({
-                    	'display'   : 'none',
-                    	'position'  : 'absolute',
-                    	'right'     : '5px',
-                    	'top'       : '5px',
-                    	'cursor'    : 'pointer'
-                	})
-               		.hover(function(){
-                    	$(this).addClass(opts.hoverClass);
-                	}, function(){
-                    	$(this).removeClass(opts.hoverClass);
-                	});
-                
-                $elt.append($ctrl)
-                	.mouseover(function(){
-                    	$('.removable-ctrl', this).show();
+					$elt.css('position', 'relative');
+				}
+				var $ctrl = $("<img class='"+opts.ctrlClass+"' src='"+opts.img+"' alt='"+opts.label+"' title='"+opts.label+"'>");
+				$ctrl.css({
+						'display'   : 'none',
+						'position'  : 'absolute',
+						'right'     : '5px',
+						'top'       : '5px',
+						'cursor'    : 'pointer'
 					})
-                	.mouseout(function(){
-                    	$('.removable-ctrl', this).hide();
-                	});
-                $ctrl.click(function(){
-                    if(confirm(opts.warning)){
-                        $elt.remove();
+			   		.hover(function(){
+						$(this).addClass(opts.hoverClass);
+					}, function(){
+						$(this).removeClass(opts.hoverClass);
+					});
+				
+				$elt.append($ctrl)
+					.mouseover(function(){
+						$('.removable-ctrl', this).show();
+					})
+					.mouseout(function(){
+						$('.removable-ctrl', this).hide();
+					});
+				$ctrl.click(function(){
+					if(confirm(opts.warning)){
 						$elt.trigger('removed.removablearea');
-                    }
-                });
+						$elt.remove();
+					}
+				});
 				$elt.trigger('init.removablearea');
 			});
 		},
@@ -54,17 +54,17 @@
 	};
 
 	$.fn.removableArea = function( method ) {
-        if (RemovableArea[method]) {
+		if (RemovableArea[method]) {
 			if(/^_/.test(method)){
-                $.error( 'Trying to call a private method ' + method + ' on jQuery.pluginName' );
+				$.error( 'Trying to call a private method ' + method + ' on jQuery.pluginName' );
 			} else {
-        		return RemovableArea[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        	}
+				return RemovableArea[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+			}
 		} else if ( typeof method === 'object' || ! method ) {
 			return RemovableArea._init.apply( this, arguments );
-        } else {
+		} else {
 			$.error( 'Method ' +  method + ' does not exist on jQuery.removableArea' );
-        }
-    };
+		}
+	};
 
 }(jQuery));
